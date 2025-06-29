66f57223b6476a7d608528ac2c94f9234ec18532
Kutez <162849091+Kutez@users.noreply.github.com>
2025-06-29 10:20:56 +0700

    Add files via upload

---
+++ Dev.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System;
+
+class Program
+{
+	[DllImport(@"add_ogg_comment.dll")]
+	public static extern int amp_AddOggComment([MarshalAs(UnmanagedType.LPStr)]string path, float mindist, float maxdist, float basevol, uint sndtype, float maxaidist);
+
+	public static void Main(string[] args)
+	{
+		Console.WriteLine(args);
+		amp_AddOggComment(args[1],30,150,1,2149580800,100);
+	}
+}
